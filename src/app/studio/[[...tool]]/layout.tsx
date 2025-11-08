0904f823fa32907607c96024b196940a58cc3a93
AnnaPrutnik <prutnik.anna@gmail.com>
2025-11-08 20:59:51 +0200

    add sanity studio

---
+++ src/app/studio/[[...tool]]/layout.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+export const metadata = {
+    title: "Sanity Studio",
+    description: "Embeded Sanity Studio",
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
+}
